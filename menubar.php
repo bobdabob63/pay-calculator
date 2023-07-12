c337b14f47826b04e7b4594d0283a291aed24a9b
bobdabob63 <bobdabob63@users.noreply.github.com>
2023-07-12 10:13:28 +1000

    - Added bootstrap  - Added header & footer files (still needs work)  - Added index & menubar (still needs work)

---
+++ menubar.php
@@ -0,0 +1,58 @@
+<header data-bs-theme="dark">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Website</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button type="button" class="nav-link btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">PHP</button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="headers#">Variables</a></li>
+                                <li><a class="dropdown-item" href="headers#">Loops</a></li>
+                                <li><a class="dropdown-item" href="headers#">If/Else</a></li>
+                                <li><a class="dropdown-item" href="headers#">Strings</a></li>
+                                <li><a class="dropdown-item" href="headers#">Array</a></li>
+                                <li><a class="dropdown-item" href="headers#">Date</a></li>
+                                <li><a class="dropdown-item" href="headers#">Chess</a></li>
+                                <li><a class="dropdown-item" href="headers#">mySQL</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button type="button" class="nav-link btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">GUI</button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="headers#">Form Elements</a></li>
+                                <li><a class="dropdown-item" href="headers#">Log Entry</a></li>
+                                <li><a class="dropdown-item" href="headers#">Guess the Number</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button type="button" class="nav-link btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">Assignment</button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="headers#">Hangman PHP</a></li>
+                                <li><a class="dropdown-item" href="headers#">Hangman JS</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</header>
